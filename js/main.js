4534cbeedd2aa5eba4401138dfdbdfe2422e13d6
RadimaS <selimova248@gmail.com>
2022-01-11 23:53:58 +0300

    create: second page

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// document.getElementById("button").style.backgroundColor = "black";
+
+// document.getElementById("button").onclick = function () {
+//   this.style.backgroundColor = "gray";
+// };
+{/* <script src="http://atuin.ru/js/art/stars.js" type="text/javascript"></script>; */}
